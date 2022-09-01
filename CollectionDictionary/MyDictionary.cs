09c74ceb6d7b0aaee047b19ff4502bcaa8824d9a
barisyaras <ismayaras@hotmail.com>
2022-09-01 06:26:27 +0300

    Add project files.

---
+++ CollectionDictionary/MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionDictionary
+{
+    class MyDictonary<TKey, TValue>
+    {
+        List<TKey> keyList;
+        List<TValue> valueList;
+
+        public MyDictonary()
+        {
+            keyList = new List<TKey>();
+            valueList = new List<TValue>();
+        }
+
+        public bool AddKeyValuePair(TKey key, TValue value)
+        {
+
+            for (int i = 0; i < keyList.Count; i++)
+            {
+                if (keyList[i].Equals(key))
+                {
+                    Console.WriteLine("This key alreadry exits.");
+                    return false;
+                }
+            }
+            keyList.Add(key);
+            valueList.Add(value);
+            Console.WriteLine("");
+            return true;
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            for (int i = 0; i < keyList.Count; i++)
+            {
+                if (keyList[i].Equals(key))
+                    return valueList[i];
+            }
+            return default;
+        }
+
+        public bool RemoveKeyValuePair(TKey key)
+        {
+            for (int i = 0; i < keyList.Count; i++)
+            {
+                if (keyList[i].Equals(key))
+                {
+                    keyList.RemoveAt(i);
+                    valueList.RemoveAt(i);
+                    Console.WriteLine("Value " + key + " has been deleted.");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CollectionDictionary/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CollectionDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> keyValue = new MyDictonary<int, string>();
+            bool isContinue = true;
+
+
+            while (isContinue)
+            {
+                int process = Menu();
+                switch (process)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter key");
+                        int key = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Please enter value");
+                        string value = Console.ReadLine();
+
+                        keyValue.AddKeyValuePair(key, value);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Please enter the key whose value you want to see");
+                        key = Convert.ToInt32(Console.ReadLine());
+
+                        keyValue.GetValue(key);
+                        Console.WriteLine(key + " " + keyValue.GetValue(key));
+                        Console.WriteLine("");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Please enter the key whose value you want to delete");
+                        key = Convert.ToInt32(Console.ReadLine());
+
+                        keyValue.RemoveKeyValuePair(key);
+                        break;
+
+                    case 4:
+                        isContinue = false;
+                        break;
+                }
+
+            }
+        }
+        static int Menu()
+        {
+            Console.WriteLine("Which of them do you want process");
+            Console.WriteLine("1: Add to dictonary");
+            Console.WriteLine("2: List dictonary");
+            Console.WriteLine("3: Delete from dictonary");
+            Console.WriteLine("4: Exit");
+            Console.WriteLine("");
+            int process = Convert.ToInt32(Console.ReadLine());
+
+            return process;
+        }
+    }
+}
